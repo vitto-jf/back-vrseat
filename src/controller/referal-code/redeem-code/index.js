a2824909412f2eccf88105b26f89bad17ca0c8c3
vittojf <vectorjs@gmail.com>
2024-08-12 15:15:44 -0400

    payment stripe

--- src/controller/referal-code/redeem-code/index.js
+++ src/controller/referal-code/redeem-code/index.js
@@ -1,37 +1,40 @@
-import  dotenv  from 'dotenv';
-import { deactivateCode, getCodesInfo } from '../../../repository/referal-code/index.js';
-import { codesValidation } from '../validate-code/index.js';
-import { addItemToUserInventory } from '../../../repository/playfab-utils/index.js';
+import dotenv from "dotenv";
+import {
+  deactivateCode,
+  getCodesInfo,
+} from "../../../repository/referal-code/index.js";
+import { codesValidation } from "../validate-code/index.js";
+import { addItemToUserInventory } from "../../../repository/playfab-utils/index.js";
 dotenv.config();
 
-export async function executeRedeemCode(req, res){
-    const codesInfo = await getCodesInfo(req.body.code);
-    console.log("executeRedeemCode" + JSON.stringify(codesInfo));
-    try {
-        if(codesValidation(codesInfo)) {
-            for (const code of codesInfo){
-                console.log("for from codes validation" + JSON.stringify((code)));
-                addItemToUserInventory(code.product_id, req.body.userId);
-                deactivateCode(code.code);
+export async function executeRedeemCode(req, res) {
+  const { userId, code } = req.body;
 
-            }
-            return res.status(200).send({
-                isSuccess: true,
-                message: "Codigo activo",
-              });
-        } else {
-            return res.status(500).send({
-                message: "El codigo se encuentra inactivo",
-                isSuccess: true,
-                });
-        }
-            
-        } catch (error) {
-            console.error(error);
-            return res.status(500).send({
-            message: "Error en la solicitud",
-            isSuccess: false,
-            error: error.response ? error.response.data : error.message,
-            });
-        }
+  const codesInfo = await getCodesInfo(code);
+  console.log("executeRedeemCode" + JSON.stringify(codesInfo));
+  try {
+    if (codesValidation(codesInfo)) {
+      for (const code of codesInfo) {
+        console.log("for from codes validation" + JSON.stringify(code));
+        addItemToUserInventory(code.product_id, userId);
+        deactivateCode(code.code);
+      }
+      return res.status(200).send({
+        isSuccess: true,
+        message: "Codigo activo",
+      });
+    } else {
+      return res.status(500).send({
+        message: "El codigo se encuentra inactivo",
+        isSuccess: true,
+      });
     }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({
+      message: "Error en la solicitud",
+      isSuccess: false,
+      error: error.response ? error.response.data : error.message,
+    });
+  }
+}
