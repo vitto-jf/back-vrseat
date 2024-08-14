d9fffd3f228bb036e3c5b682c21e05a3a72aaceb
vitto-jf <vjimenez@vrinsitu.com>
2024-08-14 17:27:48 -0400

    Agregar webhook ruta

--- src/config/stripe.js
+++ src/config/stripe.js
@@ -1,3 +1,4 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-export const sk = process.env.STRIPE_SECRET_KEY
+export const sk = process.env.STRIPE_SECRET_KEY;
+export const wh = process.env.WH_SECRET;
--- src/routes/stripe.routes.js
+++ src/routes/stripe.routes.js
@@ -1,14 +1,45 @@
 import { Router } from "express";
-
-
-
+import { sk, wh } from "../config/stripe";
+import Stripe from "stripe";
 
 const routes = Router();
 
-routes.post("/webhook",async (req,res)=>{
-    
-});
+const stripe = new Stripe(sk);
 
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
 
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, wh);
+    } catch (err) {
+      console.log(`Webhook Error: ${err.message}`);
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntentSucceeded = event.data.object;
+        console.log("Payment success");
+
+        console.log(paymentIntentSucceeded.object);
+
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
 
 export default routes;
