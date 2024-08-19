3d7cabccf647f1773f1d8180741c795b649bba55
vitto-jf <vjimenez@vrinsitu.com>
2024-08-19 09:29:05 -0400

    Agregar archivo de configuracion de pm2 y agregar variables de entorno

--- src/config/stripe.js
+++ src/config/stripe.js
@@ -1,9 +1,9 @@
-
+import dotenv from "dotenv";
+dotenv.config('/.env');
 export const sk = process.env.STRIPE_SECRET_KEY;
 
 export const wh = process.env.WH_SECRET;
 
 
-console.log('variable de entorno sin el proces .env')
 console.log(process.env.S_SECRET_KEY)
 
