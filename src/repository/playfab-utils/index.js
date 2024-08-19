7d1ce4153975b21348af891812d79be42567d5ff
vitto-jf <vjimenez@vrinsitu.com>
2024-08-19 17:59:17 -0400

    Eliminar funcion de alterar el codigo del inventario

--- src/repository/playfab-utils/index.js
+++ src/repository/playfab-utils/index.js
@@ -2,71 +2,50 @@ import { PlayFabAdmin, PlayFabClient, PlayFabServer } from "playfab-sdk";
 import { CompileErrorReport } from "../../utils/utils.js";
 import { error } from "console";
 import { playfabConfig } from "../../config/playfab.js";
-import { getDataSponsor } from "../sponsor/index.js";
+
 
 PlayFabServer.settings.developerSecretKey = playfabConfig.secretKey;
-export async function addItemToUserInventory(itemId, userId, objectData) {
-  try {
-    if (itemId === undefined || userId === undefined) throw Error("Bad params");
-    console.log(`addItemToUserInventory( ${itemId} , ${userId}`);
-    PlayFabServer.GrantItemsToUser(
-      {
-        CatalogVersion: "HLS1",
-        ItemIds: itemId,
-        PlayFabId: userId,
-      },
-      async (error, result) => {
-        console.log(result);
-        if (result !== null) {
-          console.log(result.data.ItemGrantResults);
+export async function addItemToUserInventory(itemId, userId) {
+    try {
+        if(itemId === undefined || userId === undefined) throw Error("Bad params");
+        console.log(`addItemToUserInventory( ${itemId} , ${userId}`);
+        PlayFabServer.GrantItemsToUser(
+            {
+                CatalogVersion: "HLS1",
+                ItemIds: itemId,
+                PlayFabId: userId,
+            },
+            (error, result) => {
+                console.log(result);
+                if (result !== null) {
+                    console.log(result);
+                    return result;
+                } else if (error !== null) {
+                    console.log("error: " + CompileErrorReport(error));
+                    return error;
+                };
+            }
+        );
 
-          await addCustomDataItemInventory(
-            result.data.ItemGrantResults[0].ItemInstanceId,
-            userId,
-            objectData
-          );
-          return result;
-        } else if (error !== null) {
-          console.log("error: " + CompileErrorReport(error));
-          return error;
-        }
-      }
-    );
-  } catch (error) {
-    console.log(CompileErrorReport(error));
-    return error;
-  }
-}
+        // PlayFabServer.GetUserInventory(
+        //     { PlayFabId: userId },
+        //     (error, result) => {
+        //                 console.log(playfabConfig.secretKey);
+        //                 console.log(userId);
+        //                 console.log(PlayFabServer.settings.developerSecretKey);
+        //                 console.log(PlayFabClient.settings.developerSecretKey);
+        //                 console.log(PlayFabAdmin.settings.developerSecretKey);
 
-export async function addCustomDataItemInventory(
-  ItemInstanceId,
-  userId,
-  objectData
-) {
-  try {
-    if (ItemInstanceId === undefined || userId === undefined)
-      throw Error("Bad params");
-    console.log(`MODIFY USER( ${ItemInstanceId} , ${userId}`);
-
-    PlayFabServer.UpdateUserInventoryItemCustomData(
-      {
-        ItemInstanceId: ItemInstanceId,
-        PlayFabId: userId,
-        Data: objectData,
-      },
-      (error, result) => {
-        console.log(result);
-        if (result !== null) {
-          console.log(result.data);
-          console.log(result);
-
-          return result;
-        } else if (error !== null) {
-          console.log("error: " + CompileErrorReport(error));
-          return error;
-        }
-      }
-    );
+        //                 console.log(result);
+        //                 if (result !== null) {
+        //                     console.log(result);
+        //                     return result;
+        //                 } else if (error !== null) {
+        //                     console.log("error: " + JSON.stringify(error));
+        //                     return error;
+        //                 };
+        //             }
+        //     );
   } catch (error) {
     console.log(CompileErrorReport(error));
     return error;
